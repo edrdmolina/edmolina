8552d040082eccc1b5adcd4b3074de31d8883746
Eduardo Molina <edrdmolina11@gmail.com>
2022-02-22 10:52:15 -0500

    Make About section responsive

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,28 +9,22 @@ import Portrait from '../Documents/Portrait.jpg';
 const useStyles = createUseStyles({
     About: {
         width: '100%',
-        minHeight: '600px',
         color: 'white',
         display:'flex',
         alignItems: 'center',
-        justifyContent: 'center',
-        padding: '3rem 0',
-    },
-    AboutContent: {
-        width: '100%',
-        maxWidth: '2000px',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'row',
         justifyContent: 'space-evenly',
-        alignItems: 'center'
+        padding: '3rem 0',
     },
     Type: {
         width: '35%',
 
         '& h2': {
             textAlign: 'center'
-        }
+        },
+
+        '& p:nth-of-type(2n)': {
+            textIndent: '1.5rem'
+        }   
     },
     ImageContainer: {
         borderRadius: '50%',
@@ -58,38 +52,48 @@ const useStyles = createUseStyles({
         to: {
             boxShadow: '0 5px 25px 15px #402A014F'
         },
-    }
+    },
+    '@media (max-width: 768px)': {
+        About: {
+            flexDirection: 'column-reverse',
+        },
+        Type: {
+            width: '75%',         
+        },
+
+        ImageContainer: {
+            height: '13rem'
+        }
+    },
 })
 
 function About() {
     const classes = useStyles();
     return (
         <section id='About' className={classes.About}>
-            <div className={classes.AboutContent}>
-                <div className={classes.Type}>
-                    <h2>About</h2>
-                    <p>
-                        After working in Aviation Maintenance for over six years, 
-                        I decided to take on a new challenge. With an existing 
-                        interest in creativity and engineering, I realized I can 
-                        contribute to a field that delivers solutions that grow 
-                        businesses, automate complex tasks, and provides data to 
-                        the masses.
-                    </p>
-                    <p>
-                        Since beginning my journey as a Full Stack Web Developer, 
-                        I have focused on learning popular technologies by completing 
-                        online courses and most importantly, building web applications 
-                        that I find of assistance in my life.
-                    </p>
-                </div>
-                <div className={classes.ImageContainer}>
-                    <img 
-                        src={Portrait} 
-                        alt='Portrait of Eduardo Molina Taken at Bok Towers in Lake Wales Florida'
-                        className={classes.Portrait} 
-                    />
-                </div>
+            <div className={classes.Type}>
+                <h2>About</h2>
+                <p>
+                    After working in Aviation Maintenance for over six years, 
+                    I decided to take on a new challenge. With an existing 
+                    interest in creativity and engineering, I realized I can 
+                    contribute to a field that delivers solutions that grow 
+                    businesses, automate complex tasks, and provides data to 
+                    the masses.
+                </p>
+                <p>
+                    Since beginning my journey as a Full Stack Web Developer, 
+                    I have focused on learning popular technologies by completing 
+                    online courses and most importantly, building web applications 
+                    that I find of assistance in my life.
+                </p>
+            </div>
+            <div className={classes.ImageContainer}>
+                <img 
+                    src={Portrait} 
+                    alt='Portrait of Eduardo Molina Taken at Bok Towers in Lake Wales Florida'
+                    className={classes.Portrait} 
+                />
             </div>
         </section>
     )
