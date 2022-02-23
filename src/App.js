695a839b29cc9b6d4d6e72b707a5726413b45fdb
Eduardo Molina <edrdmolina11@gmail.com>
2022-02-23 12:25:51 -0500

    Add loading page animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 // Libraries
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createUseStyles } from 'react-jss';
 import axios from 'axios';
 
 // Components
+import Loading from './Components/Loading';
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero";
 import About from './Components/About';
@@ -31,25 +32,44 @@ const useStyles = createUseStyles({
 function App() {
   const classes = useStyles();
 
+  const [loading, updateLoading] = useState(true);
+
   useEffect(() => {
+    loadingPage()
     sendMetaData()
   })
 
   async function sendMetaData() {
-    const res = await axios.put('https://multi-purpose-api.herokuapp.com/api/portfolio');
-    console.log(`Hello ${res.data.ipAddress}, welcome to my portfolio.\nDon't mind me, I'm only counting the amount of visits to my site.`)
+    if(loading) {
+      const res = await axios.put('https://multi-purpose-api.herokuapp.com/api/portfolio');
+      console.log(`Hello ${res.data.ipAddress}, welcome to my portfolio.\nDon't mind me, I'm only counting the amount of visits to my site.`)
+    } else return;
   }
 
-  return (
-    <div className={classes.App}>
-      < Navbar />
-      < Hero />
-      < About />
-      < Skills />
-      < Projects />
-      < Contact />
-    </div>
-  );
+  function loadingPage() {
+    if(loading) {
+      setTimeout(() => {
+        updateLoading(false);
+      }, 2000);
+    } else return;
+  }
+
+  if(loading) {
+    return (
+      < Loading />
+    )
+  } else {
+    return (
+      <div className={classes.App}>
+        < Navbar />
+        < Hero />
+        < About />
+        < Skills />
+        < Projects />
+        < Contact />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Loading.js
@@ -0,0 +1,53 @@
+// Libraries
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+// Icon
+import Icon from '../Documents/Logo2.png';
+
+const useStyles = createUseStyles({
+    Loading: {
+        backgroundImage: 'linear-gradient(90deg, #AEB5BF, #61788C, #BFB49E, #8C8C8C, #61788C, #AEB5BF)',
+        backgroundSize: '300%',
+        animation: '$bg-animation 30s linear 0s infinite alternate',
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        '& img': {
+            color: 'white',
+            animation: '$grow 2s linear 0s'
+        }
+    },
+
+    '@keyframes bg-animation': {
+        '0%': {
+            backgroundPosition: 'left',
+        },
+        '100%': {
+            backgroundPosition: 'right',
+        }
+    },
+
+    '@keyframes grow': {
+        from: {
+            transform: 'scale(0) rotate(45deg)',
+        },
+        to: {
+            transform: 'scale(1) rotate(0deg)'
+        }
+    }
+})
+
+function Loading() {
+    const classes = useStyles();
+    return (
+        <div className={classes.Loading}>
+            <img src={Icon} alt='logo' />
+        </div>
+    )
+}
+
+export default Loading
