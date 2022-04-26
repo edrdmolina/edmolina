3c55eaca91baffe431a20b7da3f7f7338f663476
edrdmolina <edrdmolina11@gmail.com>
2022-04-26 16:50:38 -0400

    Add check for valid ID

--- src/Helpers/scrollTo.js
+++ src/Helpers/scrollTo.js
@@ -1,8 +1,11 @@
 // Scrolls to div with Id input
 
 function scrollTo(id) {
-    let top = document.getElementById(id).getBoundingClientRect().y - 90;
-    window.scrollBy({ top, left: 0, behavior: 'smooth' });
+    const section = document.getElementById(id);
+    if (section) {
+        const top = section.getBoundingClientRect().y - 90;
+        window.scrollBy({ top, left: 0, behavior: 'smooth' });
+    }
 }
 
 export default scrollTo
